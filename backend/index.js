b71232dac12e562020e19c0bfd0cabf9ea6fa657
Harsh Ramani <ramaniharsh@gmail.com>
2026-06-20 12:03:32 +0530

    Update production API URL

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://taskmanager-app-neon.vercel.app",
     credentials: true,
   }),
 );
